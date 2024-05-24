28d9a970f907467535b452a854ea7d44bfeca44f
git961 <120348157+git961@users.noreply.github.com>
2024-05-24 12:06:54 +0900

    naosi

--- TGS2024/Define.h
+++ TGS2024/Define.h
@@ -3,14 +3,16 @@
 #define BLOCKSIZE 64
 #define BLOCK_HALFSIZE BLOCKSIZE/2
 
-#define map_blockmax_x 2560 / BLOCKSIZE
+#define FIELD_WIDTH 10240 //ワールド全体の幅 1280*8
+#define FIELD_HEIGHT 720 //ワールド全体の高さ
+
+#define map_blockmax_x FIELD_WIDTH / BLOCKSIZE
 #define map_blockmax_y 720 / BLOCKSIZE
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-#define FIELD_WIDTH 10240 //ワールド全体の幅 1280*8
-#define FIELD_HEIGHT 720 //ワールド全体の高さ
+
 
 #define WINDOW_HALFX SCREEN_WIDTH/2
 #define WINDOW_HALFY SCREEN_HEIGHT/2
