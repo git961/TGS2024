a949e84fe1f46f332a53e76625177a2a4c46f869
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2024-09-03 15:01:27 +0900

    間欠泉アニメーション作成中
    残りは打ちあがる水のアニメーション

--- TGS2024/Geyser.cpp
+++ TGS2024/Geyser.cpp
@@ -9,9 +9,13 @@ Geyser::Geyser(float set_x, float set_y)
 	width = 64.0f;
 	height = 64.0f;
 
-	geyser_img = LoadGraph("images/Stage/Gimmick/Geyser.png");
-	geyser_hill_img = LoadGraph("images/Stage/Gimmick/Geyser_Hill.png");
+	// 画像の読み込み
+	//geyser_img = LoadGraph("images/Stage/Gimmick/Geyser.png");
+	LoadDivGraph("images/Stage/Gimmick/Geyser.png", 3, 1, 3, 64, 64, geyser_img);
+	LoadDivGraph("images/Stage/Gimmick/Geyser_Hill.png", 4, 4, 1, 128, 64, geyser_hill_img);
 
+	hill_img_num = 0;
+	hill_anim_cnt = 0;
 	hill_y = location.y;
 	now_water_height = 0.0f;
 
@@ -21,13 +25,22 @@ Geyser::Geyser(float set_x, float set_y)
 	push_up_flg = false;			// 水は打ちあがらない
 	stop_water_time = 180;			// 供給後3秒かけて水が止まる
 	stop_water_flg = true;			// 水は止まっている
+
+	draw_middle_flg = false;
+	draw_under_flg = false;
 }
 
 Geyser::~Geyser()
 {
 	// 画像の削除
-	DeleteGraph(geyser_img);
-	DeleteGraph(geyser_hill_img);
+	for (int i = 0; i < 3; i++)
+	{
+		DeleteGraph(geyser_img[i]);
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		DeleteGraph(geyser_hill_img[i]);
+	}
 }
 
 // 更新処理
@@ -42,67 +55,138 @@ void Geyser::Update()
 	// 水が出ているなら
 	if (stop_water_flg == false)
 	{
-		// 水を打ち上げるなら
 		if (push_up_flg == true)
 		{
-			if (water_supply_time > 180)
-			{
-				// 上に移動
-				world.y--;
-
-				// 水の高さ
-				now_water_height++;
-			}
-			water_supply_time--;
-
-			if (water_supply_time <= 0)
-			{
-				// 水の打ち上げを止める
-				push_up_flg = false;
-
-				// 水の供給時間の再設定（6秒間）
-				water_supply_time = 360;
-			}
+			// 水を打ち上げる
+			LaunchWater();
 		}
 		else
 		{
 			// 水を止めていく
-
-			if (stop_water_time > 0)
-			{
-				// 元の高さに戻る
-				world.y++;
-				now_water_height--;
-				stop_water_time--;
-			}
-			else
-			{
-				// 水が止まった
-				stop_water_flg = true;
-
-				// 供給後から水が止まるまでの時間の再設定（3秒）
-				stop_water_time = 180;
-
-				now_water_height = 0.0f;
-			}
+			StopWater();
 		}
 
 		// 丘のY座標の計算
 		hill_y += now_water_height;
+
+		// 水のアニメーション
+		GeyserAnimation();
+		// 丘のアニメーション
+		HillAnimation();
 	}
 }
 
 // 描画処理
 void Geyser::Draw() const
 {
-	// 間欠泉画像の描画
-	DrawRotaGraph((int)location.x, (int)location.y, 1.0, 0.0, geyser_img, TRUE);
+	if (stop_water_flg == false)
+	{
+		// 間欠泉画像の描画
+		DrawRotaGraph((int)location.x, (int)location.y, 1.0, 0.0, geyser_img[0], TRUE);
+		if (draw_middle_flg)
+		{
+			DrawRotaGraph((int)location.x, (int)location.y + 64, 1.0, 0.0, geyser_img[1], TRUE);
+		}
+		if (draw_under_flg)
+		{
+			DrawRotaGraph((int)location.x, (int)location.y + 128, 1.0, 0.0, geyser_img[2], TRUE);
+		}
+	}
 
 	// 丘画像の描画
-	DrawRotaGraph((int)location.x, (int)hill_y, 1.0, 0.0, geyser_hill_img, TRUE);
+	DrawRotaGraph((int)location.x, (int)hill_y, 1.0, 0.0, geyser_hill_img[hill_img_num], TRUE);
 
 	// 頂点の確認
 	DrawBox((int)box_vertex.right_x, (int)box_vertex.upper_y, (int)box_vertex.left_x, (int)box_vertex.lower_y, 0x00ffff, FALSE);
+}
+
+// 水を打ち上げる
+void Geyser::LaunchWater()
+{
+	if (water_supply_time > 180)
+	{
+		// 上に移動
+		world.y--;
+
+		// 水の高さ
+		now_water_height++;
+	}
+	else if (water_supply_time <= 0)
+	{
+		// 水の打ち上げを止める
+		push_up_flg = false;
+
+		// 水の供給時間の再設定（6秒間）
+		water_supply_time = 360;
+	}
+
+	water_supply_time--;
+
+	if (now_water_height >= 64)
+	{
+		draw_middle_flg = true;
+	}
+	if (now_water_height >= 128)
+	{
+		draw_under_flg = true;
+	}
+}
+
+// 水を止める
+void Geyser::StopWater()
+{
+	if (stop_water_time > 0)
+	{
+		// 元の高さに戻る
+		world.y++;
+		now_water_height--;
+		stop_water_time--;
+
+		if (now_water_height <= 128)
+		{
+			draw_under_flg = false;
+		}
+		if (now_water_height <= 64)
+		{
+			draw_middle_flg = false;
+		}
+	}
+	else
+	{
+		// 水が止まった
+		stop_water_flg = true;
+
+		// 供給後から水が止まるまでの時間の再設定（3秒）
+		stop_water_time = 180;
+
+		now_water_height = 0.0f;
+		hill_img_num = 0;
+	}
+}
+
+// 水のアニメーション
+void Geyser::GeyserAnimation()
+{
+
+}
+
+// 丘のアニメーション
+void Geyser::HillAnimation()
+{
+	if (hill_anim_cnt < 10)
+	{
+		hill_anim_cnt++;
+	}
+	else
+	{
+		hill_anim_cnt = 0;
+		hill_img_num++;
+
+		if (hill_img_num > 3)
+		{
+			hill_img_num = 1;
+		}
+	}
 }
 
 // 水を噴き出す
--- TGS2024/Geyser.h
+++ TGS2024/Geyser.h
@@ -4,9 +4,11 @@
 class Geyser : public ObjectBase
 {
 private:
-	int geyser_img;						// 間欠泉の水画像
-	int geyser_hill_img;				// 間欠泉の丘画像
+	int geyser_img[3];						// 間欠泉の水画像
+	int geyser_hill_img[4];				// 間欠泉の丘画像
 
+	int hill_img_num;					// 丘の画像番号
+	int hill_anim_cnt;					// 丘のアニメーション用カウント
 	float hill_y;						// 丘のY座標
 	float now_water_height;				// 現在の水の高さ
 
@@ -17,6 +19,9 @@ private:
 	int stop_water_time;				// 供給後水を止める時間
 	bool stop_water_flg;				// 水が止まっているか？
 
+	bool draw_middle_flg;				// 2番目の水画像を描画するか？
+	bool draw_under_flg;				// 3番目の水画像を描画するか？
+
 public:
 	Geyser(float set_x, float set_y);
 	~Geyser();
@@ -24,6 +29,14 @@ public:
 	void Update();						// 更新処理
 	void Draw() const;					// 描画処理
 
+private:
+	void LaunchWater();					// 水を打ち上げる
+	void StopWater();					// 水を止める
+
+	void GeyserAnimation();				// 水のアニメーション
+	void HillAnimation();				// 丘のアニメーション
+
+public:
 	void WaterComesOut();				// 水を出す
 	bool GetStopWaterFlg() const;		// 水が出ているかの確認
 };
